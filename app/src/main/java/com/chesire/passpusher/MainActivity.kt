149c767cc7e1c4c4b646c5e72d1a3d4b8479553e
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2020-05-22 22:28:12 +0100

    docs: add kdoc to the main activity
    Remove detekt issue for missing kdoc on the public class

--- app/src/main/java/com/chesire/passpusher/MainActivity.kt
+++ app/src/main/java/com/chesire/passpusher/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.chesire.passpusher
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * The main activity for the application.
+ */
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
