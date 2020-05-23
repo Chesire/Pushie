b812a3b6b41bb648e959dba660cf6ca51f99b7ab
Troy Rijkaard <Chesire@users.noreply.github.com>
2020-05-23 18:37:47 +0100

    feat: customise options (#16)
    * feat: add controls to customise the expiry time
    
    The API allows customising the expiry time in days, so add a NumberPicker to allow the user to
    select the time. The defaults look to be 1-90 days values, with 7 being the default
    
    close #7
    
    * feat: add control to customise the expiry views
    
    The API allows customising the amount of views until expiry, add a NumberPicker to allow the user to
    select the amount. The defaults look to be 1-100 as the values, and 5 being the default
    
    close #8
    
    * refactor: space out the number picker controls
    
    Space out the number pickers within the view, so they don't just attach to the edges

--- app/src/main/java/com/chesire/passpusher/MainActivity.kt
+++ app/src/main/java/com/chesire/passpusher/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.chesire.passpusher.databinding.ActivityMainBinding
 
+private const val DAYS_PICKER_BUNDLE_KEY = "DAYS_PICKER_BUNDLE_KEY"
+private const val VIEWS_PICKER_BUNDLE_KEY = "VIEWS_PICKER_BUNDLE_KEY"
+
 /**
  * The main activity for the application.
  */
@@ -14,5 +17,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initializeViews(savedInstanceState)
+    }
+
+    private fun initializeViews(savedInstanceState: Bundle?) {
+        binding.daysPicker.apply {
+            minValue = 1
+            maxValue = 90
+            value = savedInstanceState?.getInt(DAYS_PICKER_BUNDLE_KEY) ?: 7
+        }
+        binding.viewsPicker.apply {
+            minValue = 1
+            maxValue = 100
+            value = savedInstanceState?.getInt(VIEWS_PICKER_BUNDLE_KEY) ?: 5
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(DAYS_PICKER_BUNDLE_KEY, binding.daysPicker.value)
+        outState.putInt(VIEWS_PICKER_BUNDLE_KEY, binding.viewsPicker.value)
+        super.onSaveInstanceState(outState)
     }
 }
