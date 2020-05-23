bfe552a309bd7ab7ea9cc8fed627eca63e3cf818
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2020-05-23 12:57:53 +0100

    feat: add controls to customise the expiry time
    The API allows customising the expiry time in days, so add a NumberPicker to allow the user to
    select the time. The defaults look to be 1-90 days values, with 7 being the default
    
    close #7

--- app/src/main/java/com/chesire/passpusher/MainActivity.kt
+++ app/src/main/java/com/chesire/passpusher/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.chesire.passpusher.databinding.ActivityMainBinding
 
+private const val DAYS_PICKER_BUNDLE_KEY = "DAYS_PICKER_BUNDLE_KEY"
+
 /**
  * The main activity for the application.
  */
@@ -14,5 +16,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initializeViews(savedInstanceState)
+    }
+
+    private fun initializeViews(savedInstanceState: Bundle?) {
+        binding.daysPicker.apply {
+            minValue = 1
+            maxValue = 90
+            value = savedInstanceState?.getInt(DAYS_PICKER_BUNDLE_KEY) ?: 7
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(DAYS_PICKER_BUNDLE_KEY, binding.daysPicker.value)
+        super.onSaveInstanceState(outState)
     }
 }
