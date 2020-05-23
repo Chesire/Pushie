0209938c07f2250ed582f85283c3c416cdc589c0
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2020-05-23 12:12:22 +0100

    feat: add viewbinding to the project
    Use ViewBinding to more easily access the views from within activities/fragments

--- app/src/main/java/com/chesire/passpusher/MainActivity.kt
+++ app/src/main/java/com/chesire/passpusher/MainActivity.kt
@@ -2,13 +2,17 @@ package com.chesire.passpusher
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.chesire.passpusher.databinding.ActivityMainBinding
 
 /**
  * The main activity for the application.
  */
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
