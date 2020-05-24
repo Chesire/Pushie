ed43fd0054a0f0534706ee4b51e17155fa16f332
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2020-05-24 13:48:03 +0100

    feat: add basic ui to interact with the api methods
    Add a basic button to the ui so that it can tell the viewmodel to begin executing against the api

--- app/src/main/java/com/chesire/passpusher/MainActivity.kt
+++ app/src/main/java/com/chesire/passpusher/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.chesire.passpusher
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.chesire.passpusher.api.PasswordPusher
 import com.chesire.passpusher.databinding.ActivityMainBinding
+import okhttp3.OkHttpClient
 
 private const val DAYS_PICKER_BUNDLE_KEY = "DAYS_PICKER_BUNDLE_KEY"
 private const val VIEWS_PICKER_BUNDLE_KEY = "VIEWS_PICKER_BUNDLE_KEY"
@@ -12,6 +17,21 @@ private const val VIEWS_PICKER_BUNDLE_KEY = "VIEWS_PICKER_BUNDLE_KEY"
  */
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val viewModel: MainViewModel by viewModels {
+        object : ViewModelProvider.Factory {
+            private val okHttpClient = OkHttpClient()
+            private val passwordApi = PasswordPusher(okHttpClient)
+
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                return if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+                    MainViewModel(passwordApi) as T
+                } else {
+                    throw IllegalArgumentException("ViewModel Not Found")
+                }
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +51,13 @@ class MainActivity : AppCompatActivity() {
             minValue = 1
             maxValue = 100
             value = savedInstanceState?.getInt(VIEWS_PICKER_BUNDLE_KEY) ?: 5
+        }
+        binding.press.setOnClickListener {
+            viewModel.sendApiRequest(
+                binding.passwordEditText.text.toString(),
+                binding.daysPicker.value,
+                binding.viewsPicker.value
+            )
         }
     }
 
---
+++ app/src/main/java/com/chesire/passpusher/MainViewModel.kt
@@ -0,0 +1,19 @@
+package com.chesire.passpusher
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.chesire.passpusher.api.PasswordAPI
+import kotlinx.coroutines.launch
+
+/**
+ * Main view model to use for the application.
+ */
+class MainViewModel(private val passwordPusher: PasswordAPI) : ViewModel() {
+    fun sendApiRequest(password: String, expiryDays: Int, expiryViews: Int) {
+        // check password isn't blank
+        viewModelScope.launch {
+            val result = passwordPusher.sendPassword(password, expiryDays, expiryViews)
+            val t = ""
+        }
+    }
+}
