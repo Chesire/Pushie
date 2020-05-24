91f87eca28b025a66deb9cc411bb2574351796a0
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2020-05-24 16:45:42 +0100

    docs: add kdoc to viewmodel method

--- app/src/main/java/com/chesire/passpusher/MainViewModel.kt
+++ app/src/main/java/com/chesire/passpusher/MainViewModel.kt
@@ -9,6 +9,9 @@ import kotlinx.coroutines.launch
  * Main view model to use for the application.
  */
 class MainViewModel(private val passwordPusher: PasswordAPI) : ViewModel() {
+    /**
+     * Send the input details up to the API
+     */
     fun sendApiRequest(password: String, expiryDays: Int, expiryViews: Int) {
         // check password isn't blank
         viewModelScope.launch {
