b7b1784f2372ecc14e6309d17609b021c1b64ae0
Chesire <troyrijkaard@hotmail.co.uk>
2021-05-24 20:05:57 +0100

    feat: add splash screen using the app icon
    Show a splash screen when the app is opening, only show it until the activity is ready so it doesn't
    delay the user

--- app/src/main/java/com/chesire/pushie/MainActivity.kt
+++ app/src/main/java/com/chesire/pushie/MainActivity.kt
@@ -11,6 +11,7 @@ import com.chesire.pushie.pusher.PusherFragment
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
