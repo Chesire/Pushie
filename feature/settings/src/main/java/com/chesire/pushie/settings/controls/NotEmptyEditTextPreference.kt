ed0905dd6883a7534e07af33f450d641ee657926
Chesire <troyrijkaard@hotmail.co.uk>
2021-05-25 22:38:23 +0100

    feat: store the default url is no url is provided
    If the user attempts to save the url without any value in it, then save the default value back in.
    Also to show this, set a hint on the edit text with the value of the default url which should
    hopefully show this.

--- feature/settings/src/main/java/com/chesire/pushie/settings/controls/NotEmptyEditTextPreference.kt
+++ feature/settings/src/main/java/com/chesire/pushie/settings/controls/NotEmptyEditTextPreference.kt
@@ -3,14 +3,23 @@ package com.chesire.pushie.settings.controls
 import android.content.Context
 import android.util.AttributeSet
 import androidx.preference.EditTextPreference
+import com.chesire.pushie.settings.R
 
 class NotEmptyEditTextPreference(
     context: Context,
     attrs: AttributeSet
 ) : EditTextPreference(context, attrs) {
 
+    init {
+        setOnBindEditTextListener { editText ->
+            editText.hint = context.getString(R.string.default_default_url)
+        }
+    }
+
     override fun setText(text: String?) {
-        if (!text.isNullOrBlank()) {
+        if (text.isNullOrBlank()) {
+            super.setText(context.getString(R.string.default_default_url))
+        } else {
             super.setText(text)
         }
     }
